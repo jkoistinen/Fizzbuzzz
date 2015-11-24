2187eb50d407085cb6ce3d5e81867badd5c93260
jk <jk>
2015-11-24 09:59:53 +0100

    first commit

---
+++ Oddoreven.java
@@ -0,0 +1,11 @@
+public class Oddoreven {
+
+  public static void main(String[] args) {
+    int number = Integer.parseInt(args[0]);
+    if((number % 2) == 0) {
+      System.out.println("Even number!");
+    } else {
+      System.out.println("Odd number!");
+    }
+  }
+}
